eabfc00c928b0b972c94de2a15352d10d8b04e79
Angel Rosario <arosario.us@gmail.com>
2022-03-01 18:36:19 -0500

    commit tree 6801b32277027b213eea89e667453930a631e00f

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,13 +4,28 @@ class Artist < ActiveRecord::Base
 
   def get_genre_of_first_song
     #return the genre of the artist's first saved song
+    self.genres.first
   end
 
   def song_count
     #return the number of songs associated with the artist
+    self.songs.count
   end
 
   def genre_count
     #return the number of genres associated with the artist
+    self.genres.count
   end
 end
+
+
+#! NOTE/Example:
+  # the #get_first_song method so that it returns the first song saved to the artist it's called on
+  # we now want to call #songs on the instance that the method will be called on in the future.
+  # has_many :songs creates an instance method #songs that we can call on any instance of an artist
+  # has_many :songs creates an instance method #songs that we can call on any instance of an artist
+  # Calling this method now will return an array of the artist's songs
+  #! def get_first_song
+  #!   self.songs
+  #! end
+  # self.songs.first --- will return the FIRST song
